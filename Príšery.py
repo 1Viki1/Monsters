d893cbabeb2f031d4043d78f4883e176f3632d80
1Viki1 <113436636+1Viki1@users.noreply.github.com>
2023-02-07 23:37:36 +0100

    Add files via upload

---
+++ Príšery.py
@@ -0,0 +1,57 @@
+
+import random
+cage = []
+
+class Monster:
+
+    def __init__(self, temp = []):
+        self.dna = []
+        if len(temp) == 0:
+            for i in range(5):
+                self.dna.append(random.randrange(0,10))
+        else:
+            self.dna = temp
+
+    def displayer(self):
+        print(* self.dna)
+
+    def cute(self):    #vráti stupeň krásnosti príšerky
+        return self.dna.count(1)
+
+    def __add__(self, other):  #other je druhy
+        temp = []
+        for i in range(5):
+            mut = random.randrange(0,101)    #mutants
+            if mut <13:
+                temp.append(random.randrange(0,10))
+            else:
+                par = random.randrange(0,2)    # parents
+                if par == 0:
+                    temp.append(self.dna[i])
+                else:
+                    temp.append(other.dna[i])
+        return Monster(temp)
+
+for i in range(10):
+    temp = Monster()
+    cage.append(temp)
+
+for mnozenie in range(100):
+    cage.sort(key = lambda x: x.cute(), reverse = True)
+    for prišera in cage[0:5]:
+        for i in range(5):
+            čísla = [0,1,2,3,4]
+            mutant = []
+            otec = random.choice(čísla)
+            mama = random.choice(čísla)
+            if mama == otec:
+                mama = random.choice(čísla)
+            muta = random.randrange(0,101)
+            if muta <13:
+                mutant.append(random.randrange(0,10))
+            dna = [mama, otec, mutant]
+            dieta = []
+            dieta.append(random.choice(dna))
+            cage.append(dieta)
+    for monster in cage:
+        monster.displayer()
